29c222a666711eda1e8ec960d28a2747ea283160
Palak Rani <rpalak849@gmail.com>
2026-09-14 00:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.03%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,12 +2,37 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]){
-                    swap(nums[j],nums[j+1]);
-                }
+        int low = 0, mid = 0, high = n-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
+        // int count0 = 0,count1 = 0,count2 = 0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i] == 0){
+        //         count0++;
+        //     }else if(nums[i] == 1){
+        //         count1++;
+        //     }else{
+        //         count2++;
+        //     }
+        // }
+        // for(int i=0;i<count0;i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i=count0;i<count0+count1;i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i=count0+count1;i<nums.size();i++){
+        //     nums[i] = 2;
+        // }
     }
 };
