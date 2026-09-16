c6d8c725a0992c9e27899159d5fa83c514997470
Palak Rani <rpalak849@gmail.com>
2026-09-16 09:50:40 +0530

    Time: 27 ms (66.23%) | Memory: 44.2 MB (29.35%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -7,9 +7,6 @@ public:
         for(int i=0;i<=m;i++){
             dp[i][0] = 1;
         }
-        for(int j=1;j<=n;j++){
-            dp[0][j] = 0;
-        }
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 dp[i][j] = dp[i-1][j];
