d8c240a20d456334c38a5ac0244119cfa0007d45
Palak Rani <rpalak849@gmail.com>
2026-09-17 10:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (55.61%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool solve(string s1, string s2, string s3,int i,int j,vector<vector<int>>& dp){
-        int m = s1.size();
-        int n = s2.size();
-        int x = s3.size();
-        if(i == m && j == n) return true;
-        if(dp[i][j] != -1) return dp[i][j];
-        int k = i + j;
-        bool res = false;
-        if(i < m && s1[i] == s3[k]){
-            res = solve(s1,s2,s3,i+1,j,dp);
-        }
-        if(!res && j < n && s2[j] == s3[k]){
-            res = solve(s1,s2,s3,i,j+1,dp);
-        }
-        return dp[i][j] = res;
-    }
     bool isInterleave(string s1, string s2, string s3) {
         int m = s1.size();
         int n = s2.size();
         int x = s3.size();
         if(m + n != x) return false;
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(s1,s2,s3,0,0,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        dp[0][0] = true;
+        for(int i=1;i<=m;i++){
+            dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        }
+        for(int j=1;j<=n;j++){
+            dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                int take = dp[i-1][j] && (s1[i-1] == s3[i+ j -1]);
+                int take2 = dp[i][j-1] && (s2[j-1] == s3[i + j -1]);
+                dp[i][j] = take || take2;
+            }
+        }
+        return dp[m][n];
     }
 };
