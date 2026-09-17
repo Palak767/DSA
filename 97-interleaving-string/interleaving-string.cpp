d57a888adf14b35fb4ec7d2813570346c47ef0b3
Palak Rani <rpalak849@gmail.com>
2026-09-17 09:56:02 +0530

    Time: 18 ms (16.73%) | Memory: 25.1 MB (14.11%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    bool solve(string s1, string s2, string s3,int m,int n,vector<vector<int>>& dp){
-        if(m == 0 && n == 0) return true;
-        if(dp[m][n] != -1) return dp[m][n];
-        int k = m + n - 1;
+    bool solve(string s1, string s2, string s3,int i,int j,vector<vector<int>>& dp){
+        int m = s1.size();
+        int n = s2.size();
+        int x = s3.size();
+        if(i == m && j == n) return true;
+        if(dp[i][j] != -1) return dp[i][j];
+        int k = i + j;
         bool res = false;
-        if(m > 0 && s1[m-1] == s3[k]){
-            res = solve(s1,s2,s3,m-1,n,dp);
+        if(i < m && s1[i] == s3[k]){
+            res = solve(s1,s2,s3,i+1,j,dp);
         }
-        if(!res && n > 0 && s2[n-1] == s3[k]){
-            res = solve(s1,s2,s3,m,n-1,dp);
+        if(!res && j < n && s2[j] == s3[k]){
+            res = solve(s1,s2,s3,i,j+1,dp);
         }
-        return dp[m][n] = res;
+        return dp[i][j] = res;
     }
     bool isInterleave(string s1, string s2, string s3) {
         int m = s1.size();
@@ -19,6 +22,6 @@ public:
         int x = s3.size();
         if(m + n != x) return false;
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return solve(s1,s2,s3,m,n,dp);
+        return solve(s1,s2,s3,0,0,dp);
     }
 };
