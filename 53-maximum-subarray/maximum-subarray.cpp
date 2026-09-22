576cdf7ee3644890b6924a0201c6ab50545a66d6
Palak Rani <rpalak849@gmail.com>
2026-09-22 22:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -16,3 +16,35 @@ public:
         return maxi;
     }
 };
+
+// Print subarray
+
+// int print(vector<int>& nums){
+// int n = nums.size();
+// int maxi = INT_MIN;
+// int sum = 0;
+
+// int start = 0;
+// int ansSt = -1;
+// int ansEnd = -1;
+// for(int i=0;i<n;i++){
+//     i(sum == 0){
+//         start = i;
+//     }
+//     sum += nums[i];
+//     if(sum > maxi){
+//         maxi = sum;
+//         ansSt = start;
+//         ansEnd = i;
+//     }
+//     if(sum < 0){
+//         sum = 0;
+//     }
+// }
+// cout<<"Max SubArray : [";
+// for(int i=ansSt;i<=ansEnd;i++){
+//     cout<<nums[i]<<(i == ansEnd ? "" : ", ");
+// }
+// cout<<"]"<<endl;
+// return maxi;
+// }
