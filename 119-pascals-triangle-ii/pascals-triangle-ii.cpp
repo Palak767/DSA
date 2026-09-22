408fba3f6ce011b771a35f1443b4c89a327f289c
Palak Rani <rpalak849@gmail.com>
2026-09-22 22:47:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (13.15%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>>& dp){
+        if(j == 0 || j == i) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = solve(i-1,j-1,dp) + solve(i-1,j,dp);
+    }
     vector<int> getRow(int rowIndex) {
-        vector<int> dp(rowIndex+1,0);
-        dp[0] = 1;
-        for(int i=1;i<=rowIndex;i++){
-            for(int j=i;j>0;--j){
-                dp[j] = dp[j] + dp[j-1];
-            }
+        vector<vector<int>> dp(rowIndex+1,vector<int>(rowIndex+1,-1));
+        vector<int> row(rowIndex+1);
+        for(int j=0;j<=rowIndex;j++){
+            row[j] = solve(rowIndex,j,dp);
         }
-        return dp;
+        return row;
     }
 };
